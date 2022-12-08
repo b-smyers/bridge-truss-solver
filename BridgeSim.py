73719fb04586ae4689ffc8deac0a099aa8e83fb9
D-Mello <mashpotato1485@gmail.com>
2022-12-08 17:34:59 -0500

    Started working on matrices for bridge member forces, final output is incorrect, and needs fixing. Finalized switch from 'verts and edges' to 'nodes and members'

--- BridgeSim.py
+++ BridgeSim.py
@@ -24,7 +24,8 @@ def main():
 
     #plot members
     for member in data["Members"]:
-        plotMember(member)
+        pass
+        #plotMember(member)
 
     #Add and label loads
     for load in data["Loads"]:
@@ -37,38 +38,63 @@ def main():
         #get position of both nodes
         #find inverse tan of (y2-y1)/(x2-x1)
         angleToHorizontal = math.atan((nodeInfo(node2).y-nodeInfo(node1).y)/
-                          (nodeInfo(node2).x-nodeInfo(node1).x))
+                                      (nodeInfo(node2).x-nodeInfo(node1).x))
         #get cos and sin of angle
-        matrixAngleX =  math.cos(angleToHorizontal)
-        matrixAngleY =  math.sin(angleToHorizontal)
+        matrixAngleX = math.cos(angleToHorizontal)
+        matrixAngleY = math.sin(angleToHorizontal)
         #add to angles matrix
-        trussAngleMatrix[member["memberNum"]-1, member["memberNum"]-1]
+        trussAngleMatrix[(node1-1)*2, member["memberNum"]-1] = matrixAngleX #1 to 2 cos()
+        trussAngleMatrix[(node1-1)*2+1, member["memberNum"]-1] = matrixAngleY #1 to 2 sin()
+        trussAngleMatrix[(node2-1)*2, member["memberNum"]-1] = -matrixAngleX #2 to 1 cos()
+        trussAngleMatrix[(node2-1)*2+1, member["memberNum"]-1] = -matrixAngleY #2 to 1 sin()
 
+    #Adding reaction force angles to angle matrix, all reaction force angles will be completely vertical or horizontal
+    ##The last three columns for the matrix will always be ordered {Reaction2y, Reaction1x, Reaction1y}
+    for node in constrainedNodes:
+        if nodeInfo(node).isFixed:
+            trussAngleMatrix[(nodeInfo(node).nodeNum-1)*2, trussAngleMatrix.shape[1]-1] = 1
+            trussAngleMatrix[(nodeInfo(node).nodeNum-1)*2+1, trussAngleMatrix.shape[1]-2] = 1
+        elif nodeInfo(node).isRolling:
+            trussAngleMatrix[(nodeInfo(node).nodeNum-1)*2+1, trussAngleMatrix.shape[1]-3] = 1
     print(trussAngleMatrix)
+
+    inverseTrussAngleMatrix = np.linalg.inv(trussAngleMatrix)
+
+    print(inverseTrussAngleMatrix) ## I Suspect their is an issue with these values
+
+    coeffecientMatrix = np.zeros(len(data["Nodes"])*2)
+    for load in data["Loads"]:
+        coeffecientMatrix[(load["loadNode"]-1)*2+1] = load["loadForce"]
+
+    trussForceMatrix = inverseTrussAngleMatrix.dot(coeffecientMatrix.T)
+
+    print(trussForceMatrix)
+    plotTrussForces(trussForceMatrix)
 
 class Node(object):
     def __init__(self, nodeNum):
-        self.nodeex = nodeNum
+        self.nodeNum = nodeNum
         self.position = data["Nodes"][nodeNum-1]["cords"]
         self.x = self.position[0]
         self.y = self.position[1]
         self.adjMembers = data["Nodes"][nodeNum-1]["adjMembers"]
-        self.isFixed = data["Nodes"][nodeNum-1]["fixed"]
+        self.isFixed = data["Nodes"][nodeNum-1]["fixedNode?"]
+        self.isRolling = data["Nodes"][nodeNum-1]["rollingNode?"]
 
 def nodeInfo(nodeNum: int):
     nodeInfo = Node(nodeNum)
     return nodeInfo
 
-fixedPoints = []
+constrainedNodes = []
 
 def plotNode(node):
-    if nodeInfo(node).isFixed:
+    if nodeInfo(node).isFixed or nodeInfo(node).isRolling:
         plt.plot(nodeInfo(node).x, nodeInfo(node).y, "ko")
-        fixedPoints.append(node)
+        constrainedNodes.append(node)
     else:
         plt.plot(nodeInfo(node).x, nodeInfo(node).y, "bo")
 
-        plt.annotate(node, (nodeInfo(node).x, nodeInfo(node).y), color="m")
+    plt.annotate(node, (nodeInfo(node).x, nodeInfo(node).y), color="m")
 
 def plotMember(member):
     nodeXs = nodeInfo(member["nodes"][0]).x, nodeInfo(member["nodes"][1]).x
@@ -79,12 +105,23 @@ def plotMember(member):
     plt.annotate(member["memberNum"], (centerx, centery), color="c")
 
 def plotLoad(load):
-    loadX = nodeInfo(load["loadVertex"]).x
-    loadY = nodeInfo(load["loadVertex"]).y
+    loadX = nodeInfo(load["loadNode"]).x
+    loadY = nodeInfo(load["loadNode"]).y
     plt.arrow(x=loadX, y=loadY,
-                dx=0,    dy=-load["loadForce"]/50,
+                dx=0,    dy=-load["loadNode"]/25,
                 width=0.03, color="r")
-    plt.annotate(load["loadNumber"], (loadX, (loadY+load["loadForce"]/50)/2), color="c")
+    plt.annotate(load["loadNumber"], (loadX, (loadY+load["loadNode"]/25)/2), color="c")
+
+def plotTrussForces(forces):
+    i = 0
+    for member in data["Members"]:
+        nodeXs = nodeInfo(member["nodes"][0]).x, nodeInfo(member["nodes"][1]).x
+        nodeYs = nodeInfo(member["nodes"][0]).y, nodeInfo(member["nodes"][1]).y
+        plt.plot(nodeXs, nodeYs, "g") #matplotlib wants a (x, x, ...), (y, y, ...) list for some dumb reason
+
+        centerx, centery = sum(nodeXs)/2, sum(nodeYs)/2 #Midpoint formula
+        plt.annotate(round(forces[i], 2), (centerx, centery), color="k")
+        i += 1
 
 if __name__=="__main__":
     print("Starting...")
